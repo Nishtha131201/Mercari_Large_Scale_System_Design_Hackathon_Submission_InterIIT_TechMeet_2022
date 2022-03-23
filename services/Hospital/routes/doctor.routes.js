6eca488ee331d3a3a1d96865363a76643a78a4cc
Prakhar Rajpali <rajpaliprakhar23@gmail.com>
2022-03-24 03:57:29 +0530

    Created empty services

--- services/Hospital/routes/doctor.routes.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const router = express.Router({ mergeparams: true });
-
-const controllers = require("../controllers/doctor.controllers");
-
-module.exports = router;
--- services/Patient/routes/patient.routes.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const router = express.Router({ mergeparams: true });
-
-const controllers = require("../controllers/patient.controllers");
-
-module.exports = router;
