dbe964e2e5f63b44bbda73da5453b97fdec5a9db
Rahul Aggarwal <rahulaggarwal13500@gmail.com>
2022-03-24 04:04:14 +0530

    services setup

---
+++ services/Patient/index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+const PORT = process.env.PORT || 8000;
+const app = express();
+const routes = require("./routes/patient.routes");
+const methodOverride = require("method-override");
+
+var corsOptions = {
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+};
+app.use(cors(corsOptions));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*"); //Change this later to restrict it to react app only
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, x-auth-token, Origin, Accept"
+  );
+  next();
+});
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
+
+// parse application/json
+app.use(bodyParser.json());
+app.use("/patient", routes);
+
+app.listen(PORT, (req, res) => {
+  console.log(`Server is running on Port ${PORT}`);
+});
