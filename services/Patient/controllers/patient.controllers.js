c9b753e10e9f2b94321660a67c2eb5f472a0f635
Rahul Aggarwal <rahulaggarwal13500@gmail.com>
2022-03-24 04:49:09 +0530

    api setup

---
+++ services/Patient/controllers/patient.controllers.js
@@ -0,0 +1,73 @@
+const axios = require("axios");
+
+exports.addPatient = async (req, res) => {
+  try {
+    const {
+      NHID,
+      name,
+      gender,
+      height,
+      weight,
+      dob,
+      mobile_number,
+      aadhar_number,
+      blood_group,
+      address,
+      emergency_contact_name,
+      emergency_contact_number,
+      dependant,
+      dependee,
+    } = req.body;
+    axios
+      .post("http://localhost:8000/patient/", {
+        NHID,
+        name,
+        gender,
+        height,
+        weight,
+        dob,
+        mobile_number,
+        aadhar_number,
+        blood_group,
+        address,
+        emergency_contact_name,
+        emergency_contact_number,
+        dependant,
+        dependee,
+      })
+      .then((response) => {
+        res.status(200).json({
+          status: "Success",
+          message: "New Patient Added",
+          data: response,
+        });
+      })
+      .catch((error) => {
+        res
+          .status(404)
+          .json({ status: "Failure", message: "Error in Saving data" });
+      });
+  } catch (error) {
+    return res
+      .status(424)
+      .json({ status: "Failed", message: "Request Failed" });
+  }
+};
+
+exports.getPatientDetails = (req, res) => {
+  try {
+    const NHID = req.params.NHID;
+    axios
+      .get(`http://localhost:8000/patient?NHID=${NHID}`)
+      .then((response) => {
+        res.status(200).json({ status: "Success", data: response });
+      })
+      .catch((error) => {
+        res.status(424).json({ status: "Failed", message: "Request Failed" });
+      });
+  } catch (error) {
+    return res
+      .status(424)
+      .json({ status: "Failed", message: "Request Failed" });
+  }
+};
--- services/Patient/routes/patient.routes.js
+++ services/Patient/routes/patient.routes.js
@@ -3,4 +3,7 @@ const router = express.Router({ mergeparams: true });
 
 const controllers = require("../controllers/patient.controllers");
 
+router.get("/addPatientDetails", controllers.addPatient);
+router.get(":NHID", controllers.getPatientDetails);
+
 module.exports = router;
