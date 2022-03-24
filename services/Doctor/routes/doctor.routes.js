440d56efbf6d4589a2b5f5b4e4114eea1488e455
simrangarg1234 <lakshaygarg1357@gamil.com>
2022-03-24 20:28:46 +0530

    routes added

--- services/Doctor/routes/doctor.routes.js
+++ services/Doctor/routes/doctor.routes.js
@@ -3,4 +3,10 @@ const router = express.Router({ mergeparams: true });
 
 const controllers = require("../controllers/doctor.controllers");
 
+router.post("/login", controllers.DoctorLogin);
+router.get("/:docid", controllers.getDoctor);
+router.post("/", controllers.addDoctor);
+router.post("/prescribe/:nhid", controllers.prescribe);
+router.post("/labtests/:nhid", controllers.labtest);
+
 module.exports = router;
